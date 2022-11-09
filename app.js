c5c499381c6d40cb9478eca6f2ff2ae43680abd8
MinJung SEO <seominjung6052@gmail.com>
2022-11-10 04:40:16 +0900

    str 보내기

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const request = require('request');
 const app = express()
 const port = 3000
 
+// app.get('/', async(req, res) => {
+//   let test;
+  
+//   request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=005930"},async function (error, response, body){
+//       test = JSON.parse(body)
+//       console.log(test.high)
+//       let testObj = {
+//         고점: test.high
+//       }
+//       res.send(testObj)
+//   })
+// })
+
 app.get('/', async(req, res) => {
-    let test;
-    request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=005930"},async function (error, response, body){
-        test = JSON.parse(body)
-        console.log(test.per)
-        res.send(test)
-    })
+  let test;
+  
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=005930"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.high)
+      res.send(`${test.high}`)
+  })
 })
 
   
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
