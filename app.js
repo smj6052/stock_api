450dedec248f45a91540229cc27520adb109477c
MinJung SEO <seominjung6052@gmail.com>
2022-11-10 21:56:13 +0900

    각 분야별 대표 값 추출

--- app.js
+++ app.js
@@ -65,7 +65,99 @@ app.get('/051910/sum', async(req, res) => {
   })
 })
 
+/*
+* kB 금융
+*/
+//시가
+app.get('/105560/now', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=105560"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.now)
+      res.send(`${test.now}`)
+  })
+})
+//거래량
+app.get('/105560/quant', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=105560"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.quant)
+      res.send(`${test.quant}`)
+  })
+})
+//시가총액
+app.get('/105560/sum', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=105560"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.marketSum)
+      res.send(`${test.marketSum}`)
+  })
+})
+
+/*
+* NAVER
+*/
+//시가
+app.get('/035420/now', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=035420"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.now)
+      res.send(`${test.now}`)
+  })
+})
+//거래량
+app.get('/035420/quant', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=035420"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.quant)
+      res.send(`${test.quant}`)
+  })
+})
+//시가총액
+app.get('/035420/sum', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=035420"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.marketSum)
+      res.send(`${test.marketSum}`)
+  })
+})
   
+/*
+* 카카오
+*/
+//시가
+app.get('/035720/now', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=035720"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.now)
+      res.send(`${test.now}`)
+  })
+})
+//거래량
+app.get('/035720/quant', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=035720"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.quant)
+      res.send(`${test.quant}`)
+  })
+})
+//시가총액
+app.get('/035720/sum', async(req, res) => {
+  let test;
+  request.get({uri:"https://api.finance.naver.com/service/itemSummary.nhn?itemcode=035720"},async function (error, response, body){
+      test = JSON.parse(body)
+      console.log(test.marketSum)
+      res.send(`${test.marketSum}`)
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
